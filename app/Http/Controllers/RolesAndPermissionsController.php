6f12d25fdf8170e88375c6f297f41a4caed18986
rashedraju <rashedraju.cs@gmail.com>
2022-03-16 12:02:13 +0600

    add permission controller

--- app/Http/Controllers/RolesAndPermissionsController.php
+++ app/Http/Controllers/RolesAndPermissionsController.php
@@ -2,13 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UpdateRolesAndPermissionsRequest;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
 class RolesAndPermissionsController extends Controller {
+    // render all permissions
     public function index() {
-        $permissions = Permission::all()->pluck( 'name' );
-        $rules = Role::all()->pluck( 'name' );
-        return view( 'permissions', compact( ['permissions', 'rules'] ) );
+        $permissions = Permission::all();
+        $roles = Role::all();
+        return view( 'permissions', compact( ['permissions', 'roles'] ) );
+    }
+
+    // assign roles to a permission
+    public function update( UpdateRolesAndPermissionsRequest $request, Permission $permission ) {
+        $attrs = $request->validated();
+        $roles = array_keys( $attrs );
+        $permission->syncRoles( $roles );
+
+        return redirect()->route( 'permissions' )->with( 'success', 'Permission updated!' );
     }
 }
