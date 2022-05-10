17784ef0032df9a2893f17d0cda9ce25f72059c7
rashedraju <rashedraju.cs@gmail.com>
2022-05-10 18:06:00 +0600

    fixed auth user bug on ApprovalsController

--- app/Http/Controllers/ApprovalsController.php
+++ app/Http/Controllers/ApprovalsController.php
@@ -11,19 +11,16 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
 
 class ApprovalsController extends Controller {
-    public $approvals;
-
-    public function __construct() {
-        // get approvals of logged in user
-        $user = auth()->user();
-        $this->approvals = $user->approvalRequests()->orderBy( 'id', 'desc' )->paginate( 15 );
-    }
-
     public function index() {
-        return view( 'approvals.index', ['approvals' => $this->approvals] );
+        $user = auth()->user();
+        $approvals = $user->approvalRequests()->orderBy( 'id', 'desc' )->paginate( 15 );
+
+        return view( 'approvals.index', ['approvals' => $approvals] );
     }
 
     public function show( Approval $approval ) {
+        $user = auth()->user();
+        $approvals = $user->approvalRequests()->orderBy( 'id', 'desc' )->paginate( 15 );
         $approvalable = $approval->approvalable;
         $approvalStatuses = ApprovalStatus::all();
         $preview = null;
@@ -36,7 +33,7 @@ class ApprovalsController extends Controller {
             $preview = View::make( 'components.requisition', ['requisition' => $approvalable] );
         }
 
-        return view( 'approvals.show', ['approvals' => $this->approvals, 'approval' => $approval, 'approvalStatuses' => $approvalStatuses, 'preview' => $preview] );
+        return view( 'approvals.show', ['approvals' => $approvals, 'approval' => $approval, 'approvalStatuses' => $approvalStatuses, 'preview' => $preview] );
     }
 
     public function update( Approval $approval, Request $request ) {
